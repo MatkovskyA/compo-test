ffd840cc3d96d2865981e442b87bad2ed8f13fca
MatkovskyA <a.matkovsky14@gmail.com>
2024-03-27 00:34:43 +0300

    Maded full project without adaptive

--- index.js
+++ index.js
@@ -7,3 +7,35 @@ image.addEventListener("click", () => {
   i %= images.length;
   image.src = images[i];
 });
+
+// Button caunter
+
+const btnBasket = document.querySelector("#basket-btn");
+const btnCounter = document.querySelector("#counter-btn");
+
+btnBasket.addEventListener("click", () => {
+  btnCounter.classList.add("card__buttons-caunt");
+  btnBasket.classList.add("card__buttons-hidden");
+
+  let btnPlus = document.querySelector("#plus");
+  let btnMinus = document.querySelector("#minus");
+  let countText = document.querySelector("#counter");
+  let count = 1;
+  btnPlus.addEventListener("click", () => {
+    count++;
+    countText.innerText = count + " штук";
+  });
+
+  btnMinus.addEventListener("click", () => {
+    if (count > 1) {
+      count--;
+      countText.innerText = count + " штук";
+    } else if (count <= 1) {
+      
+      btnCounter.classList.remove("card__buttons-caunt");
+      btnBasket.classList.remove("card__buttons-hidden");
+      btnMinus.isDisabled = true
+    }
+  });
+});
+
